dabd874af425975b8c11cc222f7e80fde4580146
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2025-07-04 11:20:49 +0200

    Update maximum_average_subarray.cpp

--- maximum_average_subarray.cpp
+++ maximum_average_subarray.cpp
@@ -16,3 +16,31 @@ public:
         
     }
 };
+
+//second syntax
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double curr = 0;
+        for(double i = 0; i<k; i++){
+            curr = (curr + nums[i]);
+        }
+
+        double ans = curr;
+        double start_index = 0;
+        double end_index = k;
+
+        while(end_index<nums.size()){
+            curr-=nums[start_index];
+            start_index++;
+
+            curr+=nums[end_index];
+            end_index++;
+
+            ans = max(ans, curr);
+        }
+
+        return ans/k;
+        
+    }
+};
