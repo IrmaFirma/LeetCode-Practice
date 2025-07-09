ad532d1ddcc5d9f93b8f959856c9628bc36bf153
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2025-07-09 20:20:18 +0200

    Create pairs_with_abs_diff_hashset.cpp

---
+++ pairs_with_abs_diff_hashset.cpp
@@ -0,0 +1,42 @@
+// C++ Program to count all pairs with difference equal to k
+// using Hash Map
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+int countPairs(vector<int> &arr, int k) {
+    int n = arr.size();  
+    unordered_map<int, int> freq;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+      
+        // Check if the complement (arr[i] + k)
+        // exists in the map. If yes, increment count
+        if(freq.find(arr[i] + k) != freq.end()){
+            count+=freq[arr[i] + k];
+        }
+
+      
+        // Check if the complement (arr[i] - k)
+        // exists in the map. If yes, increment count
+        if(freq.find(arr[i] - k) != freq.end()){
+            count+=freq[arr[i] - k];
+        }
+      
+        // Increment the frequency of arr[i]
+        freq[arr[i]]++;
+    }
+    return count;
+}
+
+int main() {
+    vector<int> arr = {1, 4, 1, 4, 5};
+    int k = 3;
+
+    cout << countPairs(arr, k);
+    return 0;
+}
